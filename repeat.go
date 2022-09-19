b2cd1c8b604286d55daa6bbd596463093444b620
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-09-19 17:17:32 -0400

    Benchmarking

--- repeat.go
+++ repeat.go
@@ -1,9 +1,22 @@
 package iteration
 
+import "testing"
+
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		//+= "the Add AND assignment operator", adds the right operand to the left operand & assigns the result to left operand
+		repeated += character
 	}
 	return repeated
+}
+
+// run go test -bench=.
+func BenchmarkRepeat(b *testing.B) {
+	// runs b.N times and measures how long it takes
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
 }
