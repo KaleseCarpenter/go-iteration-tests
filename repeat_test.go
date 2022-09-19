ff9d9394855df107c1597e8afa94ffbe9de58f7f
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-09-19 17:03:03 -0400

    Iterations passing

---
+++ repeat_test.go
@@ -0,0 +1,13 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated) //%q - a single-quoted character literal safely escaped with Go syntax.
+	}
+
+}
